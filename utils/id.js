8c6ba37cb8cf5c067945a7f5fbb298321169bd85
SoldacRnD <3fest.dir@gmail.com>
2025-12-01 21:20:12 +0000

    Update id.js

--- utils/id.js
+++ utils/id.js
@@ -1,2 +1,13 @@
-const { nanoid } = require('nanoid')
-module.exports = function newId(len=10){ return nanoid(len) }
+// utils/id.js
+// Simple random ID generator, no external dependencies
+
+function newId(length = 8) {
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_';
+    let out = '';
+    for (let i = 0; i < length; i++) {
+        out += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return out;
+}
+
+module.exports = newId;
